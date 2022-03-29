1f7cd7375e6a07a53566e90c0f4f2a8e51fa526e
JatinChanchalani <98803873+JatinChanchalani@users.noreply.github.com>
2022-03-29 18:23:38 +0530

    Add files via upload

---
+++ src/component/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+ function Header() {
+    return (
+        <div id='main'>
+            <div className='pr-heading'>
+                <h2>THE GYM FOR <span>HIGH IMPACT ATHLETES</span></h2>
+                <p className='details'>Buils Your Body And Fitness With Professional Touch</p>
+                <div className='header-btns'>
+                    <a href='#' className='header-btn'>JOIN US</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Header
